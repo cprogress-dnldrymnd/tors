5bf55e06c12e23a63bd00020be664621793408cb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-17 19:18:25 +0800

    changes

--- tors-child/functions.php
+++ tors-child/functions.php
@@ -8,14 +8,7 @@
 function moroko_child_theme_setup()
 {
 	load_child_theme_textdomain('moroko-child', get_stylesheet_directory() . '/languages');
+	include_once('plugins/tors-elementor/tors-elementor.php');
+
 }
 add_action('after_setup_theme', 'moroko_child_theme_setup');
-
-
-add_action('after_setup_theme', 'my_load_plugin');
-
-// This function loads the plugin.
-function my_load_plugin()
-{
-	include_once('plugins/tors-elementor/tors-elementor.php');
-}
