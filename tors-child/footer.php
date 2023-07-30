845b7e2a37c161922ddb1237275cd8fd4bf6ba63
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-30 13:22:19 +0800

    Update footer.php

--- tors-child/footer.php
+++ tors-child/footer.php
@@ -1,6 +1,90 @@
 <?php $moroko_redux_demo = get_option('redux_demo'); ?>
 <!-- Footer Area Start 
+    ====================================================== -->
+<footer class="footer-area">
+    <!-- Start:Footer Main Contents-->
+    <div class="container">
+        <div class="footer-contents">
+            <!-- Start:Footer Top Content Area (Supporters) -->
+            <div class="footer-top-sec">
+                <?php if (is_active_sidebar('footer-area-1')): ?>
+                    <?php dynamic_sidebar('footer-area-1'); ?>
+                <?php endif; ?>
+            </div>
+            <!-- End:Footer Top Content Area (Supporters) -->
+            <!-- Start:Footer Bottom Content Area  -->
+            <div class="footer-middle-sec">
+                <!-- Start:Logo Sec-->
 
+                <?php
+                $i = 0;
+                if (is_active_sidebar('footer-area-2')) {
+                    $i++;
+                }
+                if (is_active_sidebar('footer-area-3')) {
+                    $i++;
+                }
+                if (is_active_sidebar('footer-area-4')) {
+                    $i++;
+                }
+                if (is_active_sidebar('footer-area-5')) {
+                    $i++;
+                }
+                ?>
+                <?php if (is_active_sidebar('footer-area-2')): ?>
+                    <div class="<?= $i > 3 ? 'col-md-3' : 'col-md-4' ?> ">
+                        <div class="logo">
+                            <?php dynamic_sidebar('footer-area-2'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <!-- Start:Logo Sec -->
+                <!-- Start:Quick Links -->
+                <?php if (is_active_sidebar('footer-area-3')): ?>
+                    <div class="<?= $i > 3 ? 'col-md-3' : 'col-md-4' ?> ">
+                        <?php dynamic_sidebar('footer-area-3'); ?>
+                    </div>
+                <?php endif; ?>
+
+                <!-- End:Quick Links -->
+                <!-- Start:News Letter Area -->
+                <?php if (is_active_sidebar('footer-area-4')): ?>
+                    <div class="<?= $i > 3 ? 'col-md-3' : 'col-md-4' ?> ">
+                        <?php dynamic_sidebar('footer-area-4'); ?>
+                    </div>
+                <?php endif; ?>
+                <!-- Start:News Letter Area -->
+                <?php if (is_active_sidebar('footer-area-5')): ?>
+                    <!-- Start:Social Icon Area -->
+                    <div class="class="<?= $i > 3 ? 'col-md-3' : 'col-md-4' ?> ">
+                        <div class="social-icon">
+                            <?php dynamic_sidebar('footer-area-5'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <!-- End:Social Icon Area -->
+            </div>
+            <!-- End:Footer Bottom Content Area -->
+        </div>
+    </div>
+    <!-- Start:Footer Main Contents-->
+    <!-- Start:Copyright Area -->
+    <div class="copyright">
+        <div class="container">
+            <p><?php if (isset($moroko_redux_demo['footer_text'])) { ?>
+                <?php echo htmlspecialchars_decode(esc_attr($moroko_redux_demo['footer_text'])); ?>
+                <?php }
+            else { ?>
+                    <?php echo esc_html__('2022 © Moroko. All rights reserved.', 'moroko');
+            } ?></p>
+        </div>
+    </div>
+    <!-- End:Copyright Area -->
+</footer>
+<!-- =================================================
+    Footer Area End -->
 <?php wp_footer(); ?>
 </body>
 
