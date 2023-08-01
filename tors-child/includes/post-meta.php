e439a01f0c8685019cbe51724f75145c260cb28e
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-01 21:17:26 +0800

    Update post-meta.php

--- tors-child/includes/post-meta.php
+++ tors-child/includes/post-meta.php
@@ -3,3 +3,20 @@
 use Carbon_Fields\Container;
 use Carbon_Fields\Complex_Container;
 use Carbon_Fields\Field;
+
+
+/*-----------------------------------------------------------------------------------*/
+/* Header, Body and Footer Scripts
+/*-----------------------------------------------------------------------------------*/
+Container::make('theme_options', __('Get a Quote Form'))
+  ->add_fields(
+    array(
+      Field::make('complex', 'get_a_quote_form', __())
+        ->add_fields(
+          array(
+            Field::make('text', 'name', __('Name')),
+            Field::make('image', 'image', __('Image')),
+          )
+        )
+    )
+  );
