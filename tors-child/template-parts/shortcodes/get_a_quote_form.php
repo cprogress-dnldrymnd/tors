e130d6b8dcecb282db93b1987015c4dab7f12989
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-01 21:37:49 +0800

    Update get_a_quote_form.php

--- tors-child/template-parts/shortcodes/get_a_quote_form.php
+++ tors-child/template-parts/shortcodes/get_a_quote_form.php
@@ -7,7 +7,7 @@ $get_a_quote_form = carbon_get_theme_option('get_a_quote_form');
     <?php foreach ($get_a_quote_form as $key => $form) { ?>
       <div class="col-lg-4">
         <input type="checkbox" name="instruments[]" id="instrument-<?= $key ?>">
-        <label for="instrument-<?= $key ?>" class="d-flex align-items-center">
+        <label for="instrument-<?= $key ?>" class="d-flex align-items-center label-box">
           <div class="image-box">
             <img src="<?= wp_get_attachment_image_url($form['image'], 'medium') ?>" alt="<?= $form['name'] ?>">
           </div>
